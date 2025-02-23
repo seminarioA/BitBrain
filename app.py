d84f4b6c17f8b465d22374a3ccc4bed1b61c8c71
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2025-02-23 11:33:23 -0500

    Mod HTML

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
+import os 
+
+from model.predecir import calcularImg
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
+
+# Determinar si estamos en Vercel
+EN_VERCEL = os.getenv("VERCEL_ENV") is not None
+
+# Definir ruta de almacenamiento
+UPLOAD_FOLDER = "tmp" if EN_VERCEL else "static/uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/convertir", methods=['POST'])
+
+
+def getImg():
+    archivo = request.files['archivo']
+    ruta_guardado = os.path.join(UPLOAD_FOLDER, "temp.png")
+    archivo.save(ruta_guardado)
+    numCal = calcularImg(ruta_guardado)
+    return render_template('base.html', num = numCal)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model/predecir.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+
+model = load_model('static\keras\modelo_clasificador.keras')
+
+def calcularImg(image_path):
+    img = load_img(image_path, target_size=(150, 150))
+    img_array = img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    decToPorcent = (prediction[0][0])*100
+    resultadoRedondeado = round(decToPorcent, 4)
+    return(resultadoRedondeado)
